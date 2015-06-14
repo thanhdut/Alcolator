2fb755c4cf3eb3061a749d2963927ca6ae1a1671
Thanhduy Tran <thanhdut@usc.edu>
2015-06-13 19:33:39 -0500

    Checkpoint 19 complete

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Thanhduy Tran on 6/13/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
---
+++ Alcolator/Alcolator/WhiskeyViewController.m
@@ -0,0 +1,37 @@
+//
+//  WhiskeyViewController.m
+//  Alcolator
+//
+//  Created by Thanhduy Tran on 6/13/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "WhiskeyViewController.h"
+
+@interface WhiskeyViewController ()
+
+@end
+
+@implementation WhiskeyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
